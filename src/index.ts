9b9c58f38c2b55612cf4e8e4a95ef4b6e4ba501f
nff747 <nff747@users.noreply.github.com>
2026-09-12 22:35:47 -0300

    feat(exports): export public engine API, tensor abstractions, and compute shaders

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './core/SynapseEngine';
+export * from './core/TernaryTensor';
+export * from './core/BenchmarkSuite';
+export * from './utils/BitPacker';
+export * from './shaders/ternaryGemm.wgsl';
+export * from './shaders/bitpack.wgsl';
